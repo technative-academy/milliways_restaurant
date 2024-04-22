7848261c8886a56b7204a979c863e03ea05aa2ee
Helder Balbino <helderbalbino@googlemail.com>
2024-04-22 14:26:22 +0100

    added hide1 class, removed comments and console.log

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -1,16 +1,12 @@
-// find all testimonial elements
 let testimonialElements = document.querySelectorAll(".testimonial");
 console.log(testimonialElements);
 
-// loop through all testimonial elements and perform a task for each element
 testimonialElements.forEach(testimonialClickedListener);
 
-// add an event listener to each testimonial element
 function testimonialClickedListener(testimonialElement) {
   testimonialElement.addEventListener("click", elementClick);
 }
 
-// on click, do something with the selected element from the reviewObject
 function elementClick(event) {
   let reviewObject = {
     dave: "'Would go back for the free bread alone.'",
@@ -18,10 +14,8 @@ function elementClick(event) {
     brynhildr: "'Dinner here feels like a slow dance with flavors.'",
   };
 
-  // find the HTML element that was clicked
   let clickedElement = event.currentTarget;
 
   let resultElement = document.querySelector("blockquote");
   resultElement.textContent = reviewObject[clickedElement.id];
-  //   console.log(reviewObject[clickedElement.id]);
 }
